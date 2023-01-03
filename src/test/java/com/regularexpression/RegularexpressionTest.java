646fb1c415815f8094a767aa64d547ac6619c2c1
amolharde44 <amolharde98@gmail.com>
2023-01-03 12:21:36 +0530

    push

---
+++ src/test/java/com/regularexpression/RegularexpressionTest.java
@@ -0,0 +1,21 @@
+package com.regularexpression;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class RegularexpressionTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
